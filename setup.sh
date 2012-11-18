376e9a5278f3a4b13a6e108c436acf5b61571898
Michael Kitson <michaelskitson@gmail.com>
2012-11-18 08:53:14 -0800

    Making the setup script work from anywhere

--- setup.sh
+++ setup.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
-dir=`pwd | grep dotfiles`
-if [ $? -eq 0 ]; then
-    for file in `\ls -A | \egrep -v 'README|setup'`; do
-	if [ -f $file ]; then
-	    echo "Linking $file"
-	    `ln -s $dir/$file ~/$file`
-	fi
-    done
-else
-    echo 'You must execute the script from within the dotfiles folder!'
-fi
+dir="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+for file in `\ls -A $dir | \egrep -v 'README|setup'`; do
+    filename="$dir/$file"
+    if [ -f $filename ]; then
+	echo "Linking $file"
+	`ln -s $filename ~/$file`
+    fi
+done
