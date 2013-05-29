b92cdb4a5c6140df1db9339c81115b93913702cf
Michael Kitson <michaelskitson@gmail.com>
2013-05-29 17:46:11 -0400

    Change dotfiles to link to non-dotted sources

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 dir="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-files=( ".bashrc .emacs .gitconfig" )
+files=( "bashrc emacs gitconfig" )
 for file in $files; do
     filename="$dir/$file"
     if [ -f "$filename" ]; then
 	echo "Linking $file"
-	`ln -s $filename ~/$file`
+	`ln -is $filename ~/.$file`
     fi
 done
